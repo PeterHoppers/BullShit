dc625cdaecde237265b579de7becd66e3e01cdf9
Chris B <35855803+TechnicalFate@users.noreply.github.com>
2018-04-16 23:15:19 -0500

    Update BS.Java
    Removed Unecessary Method

--- BS/src/BS.java
+++ BS/src/BS.java
@@ -51,15 +51,4 @@ class BSGameController extends GameController
         }
         return;
     }
-
-    public void displayBlock()
-    {
-        System.out.println("This program lets you play the card game BS.");
-        System.out.println("The goal of the game is to discard all cards from your hand.");
-        System.out.println("You discard a certain number of card on your turn");
-        System.out.println("Since you discard face down, you can lie about what cards you played.");
-        System.out.println("If someone calls BS, and you lied, you have to take the discard pile.");
-        System.out.println("If someone calls BS, and you told the truth, they have to take the discard pile.");
-        System.out.println();
-    }
 }
